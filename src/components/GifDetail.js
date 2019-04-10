ea4fb5d035c29f7bf52fd4019f8815ce6a72eee6
rfsbarreto <rfsbarreto@gmail.com>
2019-04-09 22:25:34 -0300

    Melhora um pouco a UI (#2)

--- src/components/GifDetail.js
+++ src/components/GifDetail.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const GifDetail = ({ gif }) => {
+
   if (!gif) {
     return (
       <div className="ui placeholder">
@@ -9,15 +10,31 @@ const GifDetail = ({ gif }) => {
     );
   }
 
+  const cardDivStyle = {
+    margin: '20px',
+    span: '20px',
+    align: 'center',
+    width: `${gif.image_original_url.length}`
+  };
+
   return (
     <div>
-      <img
-        className="ui medium rounded image"
-        src={gif.fixed_width_downsampled_url}
-        alt={gif.title}
-      />
-      <br />
-      <p>{`![LGTM](${gif.image_original_url})`}</p>
+      <div class="ui centered card">
+        <div class="image">
+          <img
+            className="ui medium rounded image"
+            src={gif.fixed_width_downsampled_url}
+            alt={gif.title}
+          />
+        </div>
+        <div class="content">
+          <p>{`![LGTM](${gif.image_original_url}`}</p>
+        </div>
+        <div class="ui bottom attached button copyToken" name="copyToken" value="copy">
+          <i class="clipboard icon"></i>
+          Copy
+        </div>
+      </div>
     </div>
   );
 };
