1eb9d5755fae7c87f119af3c16421e508fe78b02
Elias Sousa <humb.eliasj@gmail.com>
2019-04-10 21:11:47 -0300

    Add copy to clipboard (#4)

--- src/components/GifDetail.js
+++ src/components/GifDetail.js
@@ -1,35 +1,52 @@
-import React from "react";
+import React, { Component } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
-const GifDetail = ({ gif }) => {
+export default class GifDetail extends Component {
+  state = { copied: false };
 
-  if (!gif) {
+  onCopyClick = () => {
+    this.setState({ copied: true });
+
+    setTimeout(() => {
+      this.setState({ copied: false });
+    }, 3000);
+  };
+
+  render() {
+    if (!this.props.gif) {
+      return <div>Loading...</div>;
+    }
+
+    const {
+      fixed_width_downsampled_url,
+      title,
+      image_original_url
+    } = this.props.gif;
+    const githubEmbed = `![LGTM](${image_original_url})`;
+    const copyButtonIcon = this.state.copied ? "check" : "clipboard";
+    const copyText = this.state.copied ? "copied!" : "copy to clipboard";
+
     return (
-      <div className="ui placeholder">
-        <div className="image" />
+      <div className="ui centered card">
+        <div className="image">
+          <img
+            className="ui medium rounded image"
+            src={fixed_width_downsampled_url}
+            alt={title}
+          />
+        </div>
+        <div className="content">
+          <div className="description" style={{ wordWrap: "break-word" }}>
+            {githubEmbed}
+          </div>
+        </div>
+        <CopyToClipboard text={githubEmbed} onCopy={this.onCopyClick}>
+          <div className="ui bottom attached button">
+            <i className={`${copyButtonIcon} icon`} />
+            {copyText}
+          </div>
+        </CopyToClipboard>
       </div>
     );
   }
-
-  return (
-    <div className="ui centered card">
-      <div className="image">
-        <img
-          className="ui medium rounded image"
-          src={gif.fixed_width_downsampled_url}
-          alt={gif.title}
-        />
-      </div>
-      <div className="content">
-        <div className="description" style={{ wordWrap:"break-word" }}>
-          {`![LGTM](${gif.image_original_url})`}
-        </div>
-      </div>
-      <div className="ui bottom attached button copyToken" name="copyToken" value="copy">
-        <i className="clipboard icon"></i>
-        Copy
-      </div>
-    </div>
-  );
-};
-
-export default GifDetail;
+}
